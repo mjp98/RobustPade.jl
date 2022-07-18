56cd1bda25f7b0ce3e9f35634cbc462a6ed9da3b
Matthew Priddin <mjp98@cam.ac.uk>
2022-07-18 11:22:31 +0100

    add BigFloat example

---
+++ examples/bigfloat.jl
@@ -0,0 +1,7 @@
+using GenericLinearAlgebra, TaylorSeries, RobustPade
+
+m,n = 20,20
+coeffs = taylor_expand(sin,big(0.);order=m+n+1).coeffs
+r = robustpade(coeffs,m,n)
+x = big(π)/12
+r(x)-sin(x) |> abs
