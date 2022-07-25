503b30a60b6893ac98dc682fd5d4617a11cda6ab
mjp98 <mjp98@users.noreply.github.com>
2022-07-25 23:41:30 +0100

    More typos

--- src/pade.jl
+++ src/pade.jl
@@ -36,7 +36,7 @@
         tol::Real=100eps()
     )
 
-computes the (m,n) Pade approximant to a function f using TaylorSeries.taylor_expand to computed the Taylor coefficients.
+computes the (m,n) Pade approximant to a function f using TaylorSeries.taylor_expand to compute the Taylor expansion.
 
 """
 function robustpade(f::Function,m::Integer,n::Integer,x=0.;kwargs...)
